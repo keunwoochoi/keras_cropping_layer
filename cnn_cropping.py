29cb15afffc9cd1343307abd51db5d44d8cbbd32
keunwoochoi <gnuchoi+github@gmail.com>
2016-07-31 02:09:42 -0400

    add files

---
+++ cnn_cropping.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.utils import np_utils
+
+import cropping	
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 4
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+nb_filters = 1
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                        border_mode='same',
+                        input_shape=(1, img_rows, img_cols)))
+
+model.add(cropping.Cropping2D(cropping=((2,2),(2,2)) ))
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                        border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(img_rows/2, img_cols/2)))
+
+model.add(Flatten())
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+
+model.summary()
+# fit
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+          verbose=1, validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
